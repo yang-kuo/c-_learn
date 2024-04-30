b5ab33cdbd62aa3ff232b121bf8f1f057a27b916
yang-kuo <ykuo329@gmail.com>
2024-04-30 09:26:20 +0800

    add new

--- lesson01/hello.cpp
+++ lesson01/hello.cpp
@@ -6,8 +6,8 @@ int main()
 {
 	int num1 = 3;
 	int num2 = 5;
-	std::cout<<"num1 + num2 = "<<num1 + num2<<"!"<<std::endl;
-	std::cout<<"this is the first cpp program!"<<std::endl;
+	std::cout << "num1 + num2 = " << num1 + num2 << "!" << std::endl;
+	std::cout << "this is the first cpp program!" << std::endl;
 	return 0;
-
 }
+
