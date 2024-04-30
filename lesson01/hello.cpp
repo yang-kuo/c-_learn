271b23a9fa5f18f38b5c099a7f44cebdf001e4dc
yang-kuo <ykuo329@gmail.com>
2024-04-30 10:16:58 +0800

    add new feature

--- lesson01/hello.cpp
+++ lesson01/hello.cpp
@@ -6,6 +6,10 @@ int main(void)
 {
 	int num1 = 3;
 	int num2 = 5; // define a number
+	int num3;
+	cout << "please input a number: " << endl;
+	cin >> num3;
+	cout << "you input number is: "<< num3 << endl;
 	std::cout << "num1 + num2 = " << num1 + num2 << "!" << std::endl;
 	std::cout << "this is the first cpp program!" << std::endl;
  	return 0;
