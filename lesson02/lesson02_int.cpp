81839491920f05b2c84e8e6135242e5ec95207f2
yang-kuo <ykuo329@gmail.com>
2024-05-09 20:22:31 +0800

    add new

--- lesson02/lesson02_int.cpp
+++ lesson02/lesson02_int.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main(void)
 {
-
+	
+	int* pt = new int;
+	*pt = 20;
+	cout <<"address ：" << pt << "value: " << *pt << endl;
 	cout<<"这是第二节课程"<<endl;
+	delete pt;
 	return 0;
 
 }
