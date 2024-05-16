6e7761735b2a40060071f80aa1d6a8b2e898ca6d
yangkuo <ykuo329@gmail.com>
2024-05-16 21:30:23 +0800

    add new

---
+++ cmake_learn/add.cpp
@@ -0,0 +1,7 @@
+#include "head.h"
+int add_test(const int* a, const int* b)
+{
+    int sum = 0;
+    sum = *a + *b;
+    return sum;
+}
---
+++ cmake_learn/head.h
@@ -0,0 +1,10 @@
+#ifndef HEAD_H__
+#define HEAD_H__
+
+#include<iostream>
+#include<string>
+using namespace std;
+int add_test(const int* a, const int* b);
+int sub_test(const int* a, const int* b);
+
+#endif
