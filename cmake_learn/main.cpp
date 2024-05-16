5564de17523c15f260acca050ce721dd9fbf2d3d
yangkuo <ykuo329@gmail.com>
2024-05-16 21:59:19 +0800

    add new

--- cmake_learn/main.cpp
+++ cmake_learn/main.cpp
@@ -5,6 +5,8 @@ int main(void)
     int* pb = new int(20);
     cout << "调用两个数的和函数结果为："<< add_test(pa, pb) << endl;
     cout << "调用两个数的差函数结果为："<< sub_test(pa, pb) << endl;
+    delete pa;
+    delete pb;
 
     return 0;
 }
