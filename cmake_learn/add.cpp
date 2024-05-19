824f2e39aee4c895583999df193fcddf3f49c075
yangkuo <ykuo329@gmail.com>
2024-05-19 20:11:09 +0800

    add new

--- cmake_learn/add.cpp
+++
@@ -1,7 +0,0 @@
-#include "head.h"
-int add_test(const int* a, const int* b)
-{
-    int sum = 0;
-    sum = *a + *b;
-    return sum;
-}
--- cmake_learn/sub.cpp
+++
@@ -1,6 +0,0 @@
-#include "head.h"
-int sub_test(const int* a, const int* b)
-{
-    int result = *a - *b;
-    return result;
-}
